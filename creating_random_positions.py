6179de50f1d3efd92ac6ec6a42d69a0fda1bf72f
astrosebas <sebascrrzgax@gmail.com>
2021-06-22 15:44:38 -0600

    Generation of random RA & DEC positions of objects

---
+++ creating_random_positions.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+RA = np.random.uniform(low=0.0, high=360.0, size=10000)
+DEC = np.random.uniform(low=-90.0, high=90.0, size=10000)
+
+positions = pd.DataFrame(RA,DEC)
+
+positions.to_csv('ob_positions.csv', header='RA,DEC')
+
